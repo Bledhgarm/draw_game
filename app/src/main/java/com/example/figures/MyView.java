a5dbaae91b1c066fb41d5253bb266833c3dccd0e
bledhgarm <kurutta.grind@gmail.com>
2020-12-25 08:17:44 +0500

    add drawin points

--- app/src/main/java/com/example/figures/MyView.java
+++ app/src/main/java/com/example/figures/MyView.java
@@ -7,39 +7,51 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.graphics.Point;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyView extends View {
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
+
     public MyView(Context context) {
         super(context);
     }
+
+    List<Point> points = new ArrayList<Point>();
+//    Point[] points = new Point[4];
     int cellSize = 30;
     int width;
     int height;
+    int countPoints;
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         width = getWidth();
         height = getHeight();
         drawGrid(canvas);
+        drawPoints(canvas);
 
     }
+
     public void drawGrid(Canvas canvas) {
         Paint paint = new Paint();
-
-        for (int i = 0; i < width/cellSize; i++) {
+        DashPathEffect effects = new DashPathEffect(new float[] { 3, 9}, 0);
+        paint.setPathEffect(effects);
+        for (int i = 0; i < width / cellSize; i++) {
             int x = i * cellSize;
             canvas.drawLine(x, 0, x, height, paint);
             paint.setColor(Color.parseColor("black"));
         }
-        for (int i = 0; i < height/cellSize; i++) {
+        for (int i = 0; i < height / cellSize; i++) {
             int y = i * cellSize;
             canvas.drawLine(0, y, width, y, paint);
         }
@@ -49,12 +61,24 @@ public class MyView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            Log.i("onTouch", event.getAction() + "");
+          downTouch(event.getX(), event.getY());
+        }
+        return true;
+    }
+    public void downTouch(float x, float y) {
+        points.add(new Point((int)x,(int)y));
+        invalidate();
+    }
+
+    public void drawPoints(Canvas canvas) {
+        Paint paint = new Paint();
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.get(i);
+            canvas.drawCircle(point.x, point.y, 5, paint);
         }
 
 
-        return true;
+
     }
+
 }
-
-
